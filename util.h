3f302d544ebad1d2508191fdfb09c8ea998eaa0f
giovaniortolani <giovaniortolanibarbosa@gmail.com>
2018-03-27 22:36:33 -0300

    update .gitignore

---
+++ util.h
@@ -0,0 +1,21 @@
+/****************************************************/
+/* File: util.h                                     */
+/* Utility functions for the TINY compiler          */
+/* Compiler Construction: Principles and Practice   */
+/* Kenneth C. Louden                                */
+/****************************************************/
+
+#ifndef _UTIL_H_
+#define _UTIL_H_
+
+/* Procedure printToken prints a token 
+ * and its lexeme to the listing file
+ */
+void printToken( TokenType, const char* );
+
+/* Function copyString allocates and makes a new
+ * copy of an existing string
+ */
+char * copyString( char * );
+
+#endif
